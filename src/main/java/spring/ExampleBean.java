c3616285674b33ee45ce186a247c02b0adbc2c35
Adam Kaniecki <adam2113@poczta.onet.pl>
2024-02-04 10:34:56 +0100

    przypadek wywołania konstuktora z arg.(czyli prawidłowe wiązanie) ale bez stosowania Autowired

--- src/main/java/spring/ExampleBean.java
+++ src/main/java/spring/ExampleBean.java
@@ -9,7 +9,8 @@ public class ExampleBean {
     private  InjectedBean injectedBean;
     private  AnotherInjectedBean anotherInjectedBean;
 
-
+//jeśli są 2 kontruktory to Spring wybierze bezargumentowy, ale jak jest jeden to Spring automatycznie go wybierze bez
+//    konieczności stosowania adnotacji @Autowired
 
     public ExampleBean(InjectedBean injectedBean, AnotherInjectedBean anotherInjectedBean) {
         this.injectedBean = injectedBean;
@@ -17,9 +18,10 @@ public class ExampleBean {
         System.out.println("Calling constructor Example Bean(2)");
     }
 
-    public ExampleBean() {
-        System.out.println("Calling constructor Example Bean()");
-    }
+
+//    public ExampleBean() {
+//        System.out.println("Calling constructor Example Bean()");
+//    }
 
     public void exampleMethod() {
         System.out.println("calling last");
