cfbe12ee67b9e15c62fee7582c2090c24bb1e51d
Adam Kaniecki <adam2113@poczta.onet.pl>
2024-02-06 23:52:14 +0100

    dołączenie pliku users.csv

--- src/main/java/spring/SomeBean1.java
+++
@@ -1,7 +0,0 @@
-package spring;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class SomeBean1 implements SomeCommonInterface {
-}
--- src/main/java/spring/SomeBean2.java
+++
@@ -1,7 +0,0 @@
-package spring;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class SomeBean2 implements SomeCommonInterface {
-}
